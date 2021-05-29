550eefad35ae47663e7f322281cd31827e62217f
kawai9126 <iroha.9126@gmail.com>
2021-05-29 19:43:08 +0900

    java

---
+++ java/app.js
@@ -0,0 +1,19 @@
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8" />
+  </head>
+  <body>
+    var orange = 100;
+    var apple = 120;
+
+    if(orange < apple){
+      alert('みかんの値段がりんごより安い');
+    } else if(orange == apple){
+      alert('みかんとりんごが同じ値段');
+    } else{
+      alert('みかんの値段がりんごより高い');
+    }
+  </body>
+</html>
