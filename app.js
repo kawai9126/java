07810542e1deb79bd0a63140f2b476a3f651e3bf
kawai9126 <iroha.9126@gmail.com>
2021-05-31 22:30:44 +0900

    java

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
- // メイン部分
-var alertString;
-alertString = addString("WebCamp");
+var promptStr = prompt('何か好きな文字を入力してください。');
 
-// 作成した関数を呼び出す
-alert(alertString);
-
-// 作成した関数
-function addString(strA){
-  var addStr = "Hello " + strA;
-  return addStr;
-}
+alert(promptStr);
